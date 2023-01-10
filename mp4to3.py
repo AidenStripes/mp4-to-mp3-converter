33e8298eff8f6364db2340c28e1fa73ad8808d21
AidenStripes <84007924+AidenStripes@users.noreply.github.com>
2023-01-10 17:02:54 +0100

    Update mp4to3.py

--- mp4to3.py
+++ mp4to3.py
@@ -1,14 +1,13 @@
-import os
 from moviepy.editor import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input", help="input filename")
-parser.add_argument("--output", help="output filename")
+parser.add_argument("-i", help="input filename path", metavar="input")
+parser.add_argument("-o", help="output filename path", metavar="output")
 args = parser.parse_args()
 
-print('inputfile:', args.input)
-print('outputfile:', args.output)
+print('inputfile:', args.i)
+print('outputfile:', args.o)
 
-video = VideoFileClip(args.input)
-video.audio.write_audiofile(args.output)
+video = VideoFileClip(args.i)
+video.audio.write_audiofile(args.o)
